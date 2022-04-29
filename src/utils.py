2b1523d2f4f3edc5e66f8cb6a3b7dced88ffec99
Egor Spirin <spirin.egor@mail.ru>
2022-04-29 21:56:55 +0300

    Enable proper logging w/o tqdm

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,12 @@
-def int_sum(a: int, b: int) -> int:
-    return a + b
+import logging as py_logging
+
+from datasets.utils import logging as ds_logging
+from transformers.utils import logging as tr_logging
+
+
+def setup_logging():
+    py_logging.basicConfig(level=py_logging.INFO)
+    tr_logging.set_verbosity_info()
+    tr_logging.disable_progress_bar()
+    ds_logging.set_verbosity_info()
+    ds_logging.disable_progress_bar()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,2 @@
-from src.utils import int_sum
-
-
 def test_int_sum():
-    assert int_sum(3, 5) == 8
+    assert 3 + 5 == 8
